4aa7ac9d0f0965ea38c35bce07056158c17ac302
rvlambda <59105388+rvlambda@users.noreply.github.com>
2022-03-21 00:03:47 +0530

    Updated

--- app.py
+++ app.py
@@ -13,7 +13,31 @@ st.set_page_config(page_title="Accident Severity Prediction App",
 
 #creating option list for dropdown menu
 options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
+
+options_cause_acc = ['Moving Backward', 'Overtaking', 'Changing lane to the left',
+       'Changing lane to the right', 'Overloading', 'Other',
+       'No priority to vehicle', 'No priority to pedestrian',
+       'No distancing', 'Getting off the vehicle improperly',
+       'Improper parking', 'Overspeed', 'Driving carelessly',
+       'Driving at high speed', 'Driving to the left', 'Unknown',
+       'Overturning', 'Turnover', 'Driving under the influence of drugs',
+       'Drunk driving']
+
+options_collision_typ = ['Collision with roadside-parked vehicles',
+       'Vehicle with vehicle collision',
+       'Collision with roadside objects', 'Collision with animals',
+       'Other', 'Rollover', 'Fall from vehicles',
+       'Collision with pedestrians', 'With Train']
+
+options_vehicle_type = ['Automobile', 'Lorry (41-100Q)', 'Other', 'Pick up upto 10Q',
+       'Public (12 seats)', 'Stationwagen', 'Lorry (11-40Q)',
+       'Public (13-45 seats)', 'Public (> 45 seats)', 'Long lorry', 'Taxi',
+       'Motorcycle', 'Special vehicle', 'Ridden horse', 'Turbo', 'Bajaj', 'Bicycle']
+
+options_junction_typ = ['No junction', 'Y Shape', 'Crossing', 'O Shape', 'Other',
+       'Unknown', 'T Shape', 'X Shape']
+
+options_age = ['na', '31-50', '18-30', 'Under 18', 'Over 51', '5']
 
 options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas',
        ' Industrial areas', 'School areas', '  Recreational areas',
@@ -29,16 +53,13 @@ options_cause = ['No distancing', 'Changing lane to the right',
        'Getting off the vehicle improperly', 'Driving at high speed',
        'Overturning', 'Turnover', 'Overspeed', 'Overloading', 'Drunk driving',
        'Unknown', 'Improper parking']
-options_vehicle_type = ['Automobile', 'Lorry (41-100Q)', 'Other', 'Pick up upto 10Q',
-       'Public (12 seats)', 'Stationwagen', 'Lorry (11-40Q)',
-       'Public (13-45 seats)', 'Public (> 45 seats)', 'Long lorry', 'Taxi',
-       'Motorcycle', 'Special vehicle', 'Ridden horse', 'Turbo', 'Bajaj', 'Bicycle']
-options_driver_exp = ['5-10yr', '2-5yr', 'Above 10yr', '1-2yr', 'Below 1yr', 'No Licence', 'unknown']
-options_lanes = ['Two-way (divided with broken lines road marking)', 'Undivided Two way',
-       'other', 'Double carriageway (median)', 'One way',
-       'Two-way (divided with solid lines road marking)', 'Unknown']
 
-features = ['hour','day_of_week','casualties','accident_cause','vehicles_involved','vehicle_type','driver_age','accident_area','driving_experience','lanes']
+options_minute = [5, 10, 15, 30, 20, 40, 45, 35, 25,  0, 50, 55]
+
+options_edu = ['Above high school', 'Junior high school', 'Elementary school',
+       'High school', 'Unknown', 'Illiterate', 'Writing & reading']
+
+features = ['hour','Cause_of_accident','Type_of_collision','Minute','Type_of_vehicle','Types_of_junction','Area_accident_occured','Age_band_of_casualty','Day_of_week','Educational_level']
 
 
 st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
@@ -47,33 +68,32 @@ def main():
 
         st.subheader("Enter the input for following features:")
         
-        hour = st.slider("Pickup Hour: ", 0, 23, value=0, format="%d")
-        day_of_week = st.selectbox("Select Day of the Week: ", options=options_day)
-        casualties = st.slider("Hour of Accident: ", 1, 8, value=0, format="%d")
-        accident_cause = st.selectbox("Select Accident Cause: ", options=options_cause)
-        vehicles_involved = st.slider("Pickup Hour: ", 1, 7, value=0, format="%d")
-        vehicle_type = st.selectbox("Select Vehicle Type: ", options=options_vehicle_type)
-        driver_age = st.selectbox("Select Driver Age: ", options=options_age)
-        accident_area = st.selectbox("Select Accident Area: ", options=options_acc_area)
-        driving_experience = st.selectbox("Select Driving Experience: ", options=options_driver_exp)
-        lanes = st.selectbox("Select Lanes: ", options=options_lanes)
-        
-        
+        hour = st.slider("Hour of Accident: ", 0, 23, value=0, format="%d")
+        Cause_of_accident = st.selectbox("Select Cause of Accident : ", options=options_cause_acc)
+        Type_of_collision = st.selectbox("Select Type Of Collision : ", options=options_collision_typ)
+        Minute = st.selectbox("Select Minute of Accident : ", options=options_minute)
+        Type_of_vehicle = st.selectbox("Select type of vehicle : ", options=options_vehicle_type)
+        Types_of_junction = st.selectbox("Select type of vehicle : ", options=options_junction_typ)
+        Area_accident_occured = st.selectbox("Select Accident Area: ", options=options_acc_area)
+        Age_band_of_casualty = st.selectbox("Select Age of casualty: ", options=options_age)
+        Day_of_week = st.selectbox("Select Day of the Week: ", options=options_day)
+        Educational_level = st.selectbox("Select Cause of Accident : ", options=options_edu)
+
         submit = st.form_submit_button("Predict")
 
 
     if submit:
-        day_of_week = ordinal_encoder(day_of_week, options_day)
-        accident_cause = ordinal_encoder(accident_cause, options_cause)
-        vehicle_type = ordinal_encoder(vehicle_type, options_vehicle_type)
-        driver_age =  ordinal_encoder(driver_age, options_age)
-        accident_area =  ordinal_encoder(accident_area, options_acc_area)
-        driving_experience = ordinal_encoder(driving_experience, options_driver_exp) 
-        lanes = ordinal_encoder(lanes, options_lanes)
+        Cause_of_accident = ordinal_encoder(Cause_of_accident, options_cause_acc)
+        Type_of_collision = ordinal_encoder(Type_of_collision, options_collision_typ)
+        Type_of_vehicle = ordinal_encoder(Type_of_vehicle, options_vehicle_type)
+        Types_of_junction = ordinal_encoder(Types_of_junction, options_junction_typ)
+        Area_accident_occured = ordinal_encoder(Area_accident_occured, options_acc_area)
+        Age_band_of_casualty = ordinal_encoder(Age_band_of_casualty, options_age)
+        Day_of_week = ordinal_encoder(Day_of_week, options_day)
+        Educational_level = ordinal_encoder(Educational_level, options_edu)
 
-
-        data = np.array([hour,day_of_week,casualties,accident_cause,vehicles_involved, 
-                            vehicle_type,driver_age,accident_area,driving_experience,lanes]).reshape(1,-1)
+        data = np.array([hour,Cause_of_accident,Type_of_collision,Minute,Type_of_vehicle, 
+                            Area_accident_occured,Age_band_of_casualty,Day_of_week,Educational_level]).reshape(1,-1)
 
         pred = get_prediction(data=data, model=model)
 
